d07bc14be3e6eb95151e0fef689926b268ab2439
ericbanker12 <ericbanker12@gmail.com>
2019-09-10 14:37:03 -0500

    import data as state

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -4,8 +4,11 @@ import React from "react";
 
 //Import your array data to from the provided data file
 
+import {operators} from '../../../data'
+
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operatorState, setOperatorState] = React.useState(operators)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
